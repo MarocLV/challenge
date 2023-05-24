4efaaf15e22982f7be53f0ef68ca0b68e1a3337d
MarocLV <marco_321vergara@hotmail.com>
2023-05-24 00:40:23 -0700

    csrf conf

--- core/settings.py
+++ core/settings.py
@@ -27,6 +27,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
+CSRF_TRUSTED_ORIGINS = [
+    'https://challenge-production-eb53.up.railway.app',
+]
+
 CORS_ALLOWED_ORIGINS = [
     'https://challenge-production-eb53.up.railway.app',
 ]
