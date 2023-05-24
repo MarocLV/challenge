51a5cf762d3207d91b05233fc36eaddf1fd6db94
MarocLV <marco_321vergara@hotmail.com>
2023-05-24 00:05:37 -0700

    config db

--- core/settings.py
+++ core/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'cfqreWgeaDd4wYL3K8p6',
+        'HOST': 'containers-us-west-35.railway.app',  # Si la base de datos se encuentra en el mismo servidor
+        'PORT': '7822',  # Puerto por defecto de PostgreSQL
     }
 }
 
